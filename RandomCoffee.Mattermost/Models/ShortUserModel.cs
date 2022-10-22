ad278ddfee192bc18effa3a90cd6fa3e7f2bb026
Yuliya Lytkina <y.lytkina@junior-projects.com>
2022-10-22 11:45:17 +0600

    Added models for Mattermost users

---
+++ RandomCoffee.Mattermost/Models/ShortUserModel.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace RandomCoffee.Mattermost.Models
+{
+    /// <summary>
+    /// Mattermost user short info model
+    /// </summary>
+    public class ShortUserModel
+    {
+        [JsonProperty("user_id")]
+        public string Id { get; set; }
+    }
+}
---
+++ RandomCoffee.Mattermost/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace RandomCoffee.Mattermost.Models
+{
+    /// <summary>
+    /// Mattermost user info model
+    /// </summary>
+    public class UserModel
+    {
+        [JsonProperty("id")]
+        public string Id { get; set; }
+        
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+    }
+}
