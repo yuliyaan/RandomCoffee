c14efd6ae34b4c848aceb84c5af3f720c672b13b
Yuliya Lytkina <y.lytkina@junior-projects.com>
2022-10-22 11:44:26 +0600

    Changed solution structure

--- RandomCoffee.Tests/Tests.cs
+++ RandomCoffee.Tests/Tests.cs
@@ -4,6 +4,15 @@ namespace RandomCoffee.Tests
 {
     public class Tests
     {
-      
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
     }
 }
