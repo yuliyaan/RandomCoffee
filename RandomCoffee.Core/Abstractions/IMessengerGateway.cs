21ec52b434096b2fcb84d640c5b025f8193502b6
Yuliya Lytkina <y.lytkina@junior-projects.com>
2022-10-22 11:21:43 +0600

    Created interfaces for MessengerGateway and Historian

---
+++ RandomCoffee.Core/Abstractions/IMessengerGateway.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RandomCoffee.Core.Abstractions
+{
+    /// <summary>
+    /// Connector for Random Coffee to a specific messenger 
+    /// </summary>
+    public interface IMessengerGateway
+    {
+        /// <summary>
+        /// Get list of Random Coffee participants
+        /// </summary>
+        /// <returns>The collection of Random Coffee's participants ids</returns>
+        Task<string[]> GetRandomCoffeeParticipantIds();
+
+        /// <summary>
+        /// Send message with Random Coffee's toss info
+        /// </summary>
+        /// <param name="groups">Random Coffee participants groups info. Each group contains 2 or 3 participants.</param>
+        /// <returns>true if message successfully sent</returns>
+        Task<bool> SendRandomCoffeeMessage(List<string[]> groups);
+    }
+}
---
+++ RandomCoffee.Core/Abstractions/IRandomCoffeeHistorian.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace RandomCoffee.Core.Abstractions
+{
+    /// <summary>
+    /// Represents a storage of Random Coffee's info
+    /// </summary>
+    public interface IRandomCoffeeHistorian
+    {
+        /// <summary>
+        /// Get info about previous tosses of Random Coffee
+        /// </summary>
+        /// <param name="count">Count of tosses to get info of (starting from the recent one)</param>
+        /// <returns>The collection of previous tosses infos. Each toss info is represented by collection of string arrays.
+        /// The arrays contain ids of Random Coffee participants, which were in one group of two or three in that toss.</returns>
+        List<List<string[]>> GetRandomCoffeeHistory(int count);
+
+        /// <summary>
+        /// Save info about one Random Coffee's toss
+        /// </summary>
+        /// <param name="groups">This toss info. Expected the collection of string arrays, each array - one group of two or three participants in that toss.</param>
+        void SaveRandomCoffeeInfo(List<string[]> groups);
+    }
+}
